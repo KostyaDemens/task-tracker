cf28c246ee2a19079d29931dd6a7817b182c9b98
kostadementev <kostyademens@gmail.com>
2025-07-16 19:43:35 +0300

    Initial commit

---
+++ task-tracker-backend/src/main/java/by/bsuir/kostyademens/tasktrackerbackend/TaskTrackerBackendApplication.java
@@ -0,0 +1,17 @@
+package by.bsuir.kostyademens.tasktrackerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.function.Function;
+
+@SpringBootApplication
+public class TaskTrackerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskTrackerBackendApplication.class, args);
+    }
+
+}
