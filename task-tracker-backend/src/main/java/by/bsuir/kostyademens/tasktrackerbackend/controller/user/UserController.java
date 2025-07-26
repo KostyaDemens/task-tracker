b6f8abd9871f8f656645d431e317739fa46d5809
kostyademens <kostyademens@gmail.com>
2025-07-26 22:33:38 +0300

    feat: add UserController to get current - authenticated user

--- task-tracker-backend/src/main/java/by/bsuir/kostyademens/tasktrackerbackend/controller/user/UserController.java
+++ task-tracker-backend/src/main/java/by/bsuir/kostyademens/tasktrackerbackend/controller/user/UserController.java
@@ -1,11 +1,10 @@
 package by.bsuir.kostyademens.tasktrackerbackend.controller.user;
 
 import by.bsuir.kostyademens.tasktrackerbackend.dto.CurrentUserDto;
+import by.bsuir.kostyademens.tasktrackerbackend.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,11 +14,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
+    private final UserService userService;
 
 
     @GetMapping("/me")
-    public ResponseEntity<String> getCurrentUser(Authentication authentication) {
-        return ResponseEntity.ok(authentication.getName());
+    public ResponseEntity<CurrentUserDto> getCurrentUser(Authentication authentication) {
 
+        CurrentUserDto currentUserDto = userService.getCurrentUser(authentication);
+
+        return ResponseEntity.ok(currentUserDto);
     }
 }
