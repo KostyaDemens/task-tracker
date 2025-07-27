11464659c07b4d4432bd82ee35749b9cdbf9ee93
kostyademens <kostyademens@gmail.com>
2025-07-27 18:26:04 +0300

    refactor: add special Http status codes for registration & logout

--- task-tracker-backend/src/main/java/by/bsuir/kostyademens/tasktrackerbackend/controller/user/AuthenticationController.java
+++ task-tracker-backend/src/main/java/by/bsuir/kostyademens/tasktrackerbackend/controller/user/AuthenticationController.java
@@ -5,8 +5,9 @@ import by.bsuir.kostyademens.tasktrackerbackend.dto.UserRegisterDto;
 import by.bsuir.kostyademens.tasktrackerbackend.model.User;
 import by.bsuir.kostyademens.tasktrackerbackend.service.AuthenticationService;
 import by.bsuir.kostyademens.tasktrackerbackend.service.JwtService;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
@@ -21,33 +22,37 @@ public class AuthenticationController {
     private final AuthenticationService authenticationService;
 
     @PostMapping("/register")
-    public ResponseEntity<Void> register(@RequestBody UserRegisterDto userRegisterDto, HttpServletResponse response) {
+    public ResponseEntity<Void> register(@RequestBody UserRegisterDto userRegisterDto) {
         User user = authenticationService.register(userRegisterDto);
 
         String jwtToken = jwtService.generateToken(user);
 
-        response.setHeader("Authorization", "Bearer " + jwtToken);
-
-        return ResponseEntity.ok().build();
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + jwtToken)
+                .build();
     }
 
     @PostMapping("/login")
-    public ResponseEntity<Void> authenticate(@RequestBody UserLoginDto userLoginDto, HttpServletResponse response) {
+    public ResponseEntity<Void> authenticate(@RequestBody UserLoginDto userLoginDto) {
         User user = authenticationService.authenticate(userLoginDto);
 
         String jwtToken = jwtService.generateToken(user);
 
-        response.setHeader("Authorization", "Bearer " + jwtToken);
-
-        return ResponseEntity.ok().build();
+        return ResponseEntity
+                .ok()
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + jwtToken)
+                .build();
     }
 
     @DeleteMapping("/logout")
-    public ResponseEntity<Void> logout(HttpServletResponse response) {
-        response.setHeader("Authorization", "");
+    public ResponseEntity<Void> logout() {
         SecurityContextHolder.clearContext();
 
-        return ResponseEntity.ok().build();
+        return ResponseEntity
+                .status(HttpStatus.NO_CONTENT)
+                .header(HttpHeaders.AUTHORIZATION, "")
+                .build();
 
         //TODO Проверить, какие коды доступа везде возвращаем
     }
